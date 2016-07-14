46b4b808b8ae8af919ba8d5cc80028be4aeac7ce
Alfonso de la Vega <alfonsodelavegaruiz@gmail.com>
2016-07-14 16:56:38 +0200

    PEP8ed

--- agente.py
+++ agente.py
@@ -2,29 +2,33 @@
 # -*- coding: utf-8 -*-
 # Ronny Conde at Monkey from the Future
 
+
 def valor_estado(estado, valores_q):
     """ Calculara y devolvera el valor asociado a estado teniendo en cuenta
     valores_q.
-    """ 
+    """
     pass
+
 
 def imprimir_valores_estados(estados, valores_q):
     """ imprime por pantalla los valores asociados a cada estado."""
     pass
 
+
 def qlearning():
     gamma = 0.9
     alpha = 0.01
-    N = 20000 # Numero de episodios
+    N = 20000  # Numero de episodios
 
     # Inicializamos estados como una lista que almacena cada posible posicion
     # del agente en el laberinto. Hay que tener en cuenta que (1, 1) no estara
     # en la lista al ser un obstaculo.
-    # E.g. estados se inicializara como [(0, 0), (0, 1), ... ] 
+    # E.g. estados se inicializara como [(0, 0), (0, 1), ... ]
     pass
 
     # valores_q sera un diccionario:
-    #   - Claves. Tuplas con la combinacion de cada estado y sus acciones disponibles
+    #   - Claves. Tuplas con la combinacion de cada estado y sus
+    #     acciones disponibles
     #   - Valores: Suma de las recompensas que se esperan recibir hasta el final
     #   del episodio si se actua de manera optima partiendo del estado Q (la
     #   clave asociada) y teniendo en cuenta el factor de descuento gamma.
--- entorno.py
+++ entorno.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # Ronny Conde at Monkey from the Future
 
+
 def acciones_disponibles(estado):
     """ Devuelve una lista con las posibles acciones que el agente puede
     seleccionar desde estado.
-    
+
     E.g. acciones_disponibles((0,0)) devolvera ['W', 'N', 'E', 'S'];
     acciones_disponibles((1,3)) devolvera ['EXIT']
     """
@@ -14,10 +15,11 @@ def acciones_disponibles(estado):
     else:
         return ['W', 'N', 'E', 'S']
 
+
 def recompensa(estado, accion):
     """ Devuelve la recompensa que se recibira al abandonar estado tras
     seleccionar accion.
-    
+
     E.g. recompensa((0,0), 'E') devolvera 0.0; recompensa((0,3), 'EXIT')
     devolvera 1.0
     """
@@ -28,13 +30,15 @@ def recompensa(estado, accion):
     else:
         return 0.0
 
+
 def reset():
     """ Devuelve el estado inicial (equivalente a reiniciar el entorno). En este
     caso el estado inicial va a ser (2, 0).
-    
+
     E.g. reset() devolvera (2, 0)
     """
     pass
+
 
 def paso(estado, accion):
     """ El entorno recibe la accion seleccionada por el agente desde estado y
@@ -42,29 +46,31 @@ def paso(estado, accion):
         - el nuevo estado
         - la recompensa (funcion de estado y accion)
         - una variable que indica si ha terminado el episodio (True / False).
-    
+
     E.g. paso((0, 0), 'E') puede devolver ((0, 1), 0.0, False)
     o ((0, 0), 0.0, False) o ((1, 0), 0.0, False)
     """
     pass
+
 
 def desplazamiento(accion):
     """ Devuelve un posible desplazamiento tras seleccionar accion (en esta
     funcion no puede ser 'EXIT'). El desplazamiento coincidira con accion el 80%
     de las veces, se desviara a la derecha, respecto a la orientacion de la
     accion, un 10% y a la izquierda otro 10%.
-    
+
     E.g. desplazamiento('W') devolvera 'W' el 80% de las veces, 'N' el 10% de
     las veces y 'S' el 10% restante.
     """
     pass
+
 
 def siguiente_estado(estado, accion):
     """ Devuelve la celda en la que acabara el agente tras elegir accion desde
     estado. La componente estocástica se ha tenido en cuenta en la función
     desplazamiento(accion). Hay que contemplar el comportamiento ante los
     obstaculos y limites del laberinto.
-    
+
     E.g. siguiente_estado((0, 1), 'E') puede devolver (0, 2) o (0, 1)
     """
     pass
